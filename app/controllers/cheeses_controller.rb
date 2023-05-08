7ce885609c8abaffd2eaa15ee57f4165c53af3d8
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-05-08 21:20:05 +0300

    commit tree cb86acef52f3cc81f9d7566032433320284c098c

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,16 @@
 class CheesesController < ApplicationController
 
+  # GET /cheeses
   def index
     cheeses = Cheese.all
     render json: cheeses
+  end
+
+  #find a cheese using the ID from the URL
+  #send a JSON response using that cheese object
+  def show
+   cheese = Cheese.find_by(id: params[:id])
+   render json: cheese
   end
 
 end
