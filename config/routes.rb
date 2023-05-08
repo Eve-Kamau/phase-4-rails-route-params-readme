6cfa31e3b08025ec319e167d18290eaaecfb2bf6
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-05-08 21:51:53 +0300

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
   get '/cheeses', to: 'cheeses#index'
+
+  #DYNAMIC ROUTE
+  get '/cheeses/:id', to: 'cheeses#show'
+
+  #STATIC ROUTE
+  # get '/cheeses/1', to: 'cheeses#first' 
+
 end
